a583e3f732b175415d179d6c3182464ca797601e
PedroA27 <94874934+PedroA27@users.noreply.github.com>
2022-01-14 19:01:17 -0300

    Update Server.java

--- server/src/main/java/br/usp/each/typerace/server/Server.java
+++ server/src/main/java/br/usp/each/typerace/server/Server.java
@@ -9,35 +9,129 @@ import java.util.Map;
 
 public class Server extends WebSocketServer {
 
+    private Jogo jogo;
     private final Map<String, WebSocket> connections;
 
-    public Server(int port, Map<String, WebSocket> connections) {
+    public Server(int port, Map<String, WebSocket> connections) {//ok
         super(new InetSocketAddress(port));
         this.connections = connections;
+        this.jogo = new Jogo();
     }
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
-        // TODO: Implementar
+        System.out.println("Nova conexao recebida");
+        if (duplicado(conn.getResourceDescriptor().substring(1))) {
+            conn.send("Este ID ja esta sendo utilizado!");
+            conn.close(1008, "Nome duplicado");
+        }
+        else{
+            String user = conn.getResourceDescriptor().substring(1);
+            connections.put(user, conn);
+            if (jogo.getEstado()) {
+                conn.send("A partida ja está sendo realizada, aguarde a proxima.");
+                return;
+            }
+            broadcast(user + " acabou de entrar!\n" + connections.size()+" %d jogadores esperando.\n" );
+            printMenu(conn);
+            System.out.println("Conexao realizada");
+        }
+
     }
 
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-        // TODO: Implementar
+
+        String jogador = conn.getResourceDescriptor().substring(1);
+        System.out.println(jogador +" desconectado.");
+        if(jogo.getEstado()){//tira jogador do meio da partida
+            jogo.removeJogador(jogo.getJogador(jogador));
+        }
+        connections.remove(jogador);
+        if(!conn.isClosed()){
+            conn.close(code, reason);
+        }
     }
 
     @Override
     public void onMessage(WebSocket conn, String message) {
-        // TODO: Implementar
+        if (message.length() == 0) return;
+        String jogador = conn.getResourceDescriptor().substring(1);
+
+        if(jogo.getEstado()){
+            inputJogando(conn,jogador,message);
+            return;
+            //input no jogo
+        }
+        String resposta = message;
+        switch(Integer.parseInt(resposta)){
+            case 1://start
+                broadcast(jogador + " começou o jogo\n");
+                jogo.inicia(connections.keySet());
+                broadcast(jogo.palavraExibida(jogador));
+                break;
+            case 2:// quit
+                System.out.printf("%s saiu do jogo com sucesso!\n", jogador);
+                if(jogador.isEmpty()) return;
+                connections.remove(jogador,conn);
+                conn.close(1000,"Saida padrao");
+                break;
+            default:
+                conn.send("Comando inexistente.");
+        }
     }
 
     @Override
     public void onError(WebSocket conn, Exception ex) {
-        // TODO: Implementar
+        if (conn != null) {
+            System.out.println("Erro em: "+ conn.getResourceDescriptor().substring(1));
+        }
+        ex.printStackTrace();
+
     }
 
     @Override
     public void onStart() {
-        // TODO: Implementar
+        System.out.println("Servidor iniciado");
     }
+
+    //Verifica ids duplicados
+    public boolean duplicado(String nome) {
+        if(connections.containsKey(nome)){
+            return true;
+        }
+        return false;
+    }
+
+    //Imprime menu do jogo
+    public void printMenu (WebSocket conn) {
+        conn.send("\nBem Vindo ao TYPE RACE\n"+"Digite o comando para prosseguir\n"+"1 - Para comecar o jogo\n"+"2 - Para sair do jogo\n");
+
+    }
+
+    //Pega inputs dos player que estão jogando
+    public void inputJogando(WebSocket conn, String user, String message) {
+        if (jogo.getJogadoresParticipando() == 0)  {
+            jogo.termina();
+            broadcast(jogo.geraRanking());
+            return;
+        }
+        if (jogo.getJogador(user) == null){
+            return;
+        }
+        if (jogo.getJogador(user).getJogando()){
+            jogo.comparaPalavra(user, message);
+        }
+        if (!jogo.getEstado()){
+            broadcast(jogo.geraRanking());
+            return;
+        }
+        if (!jogo.getJogador(user).getJogando()) {
+            conn.send("Você acabou, aguarde os outros jogadores");
+        } else {
+            conn.send(jogo.palavraExibida(user));
+        }
+    }
+
+
 }
