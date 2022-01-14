c445892d762d3f36eaf042bfcfc6ad69081162dd
PedroA27 <94874934+PedroA27@users.noreply.github.com>
2022-01-14 19:04:10 -0300

    Update Player.java

--- server/src/main/java/br/usp/each/typerace/server/Player.java
+++ server/src/main/java/br/usp/each/typerace/server/Player.java
@@ -1,36 +1,55 @@
 package br.usp.each.typerace.server;
 
-import java.util.ArrayList;
-import java.util.UUID;
+public class Jogador {
+    private String jogador;
+    private boolean jogando;
+    private int acertos;
+    private int posicaoLista;
+    private long tempo;
+    private int erros;
 
-//classe representando o jogador
-public class Player {
-    private UUID playerUId;
-    private int score;
-    private int wrong;
-    private ArrayList<String> words;
-
-    public void addScore(){
-        score++;
+    Jogador(String id){
+        this.jogador = id;
+        this.jogando = true;
+        this.acertos = 0;
+        this.erros = 0;
+        this.posicaoLista = 0;
+        this.tempo = 0;
+    }
+    public boolean getJogando(){
+        return this.jogando;
+    }
+    public int getErros() {
+        return this.erros;
+    }
+    public void proximaPalavra(){
+        this.posicaoLista++;
+    }
+    public void setPosicaoDaLista(int i) {
+        this.posicaoLista = i;
+    }
+    public int getposicaoLista() {
+        return posicaoLista;
+    }
+    public void adicionaAcerto() {
+        acertos++;
+    }
+    public void adicionaErro() {
+        erros++;
+    }
+    public int getAcertos() {
+        return acertos;
+    }
+    public long getTempo() {
+        return tempo;
+    }
+    public String getNome() {
+        return jogador;
+    }
+    public void fim(long inicio) {
+        this.tempo = (System.nanoTime() - inicio)/1000000000;
+        this.jogando = false;
     }
 
-    public void addWrong(){
-        wrong++;
-    }
-
-    public UUID getPlayerUId() {
-        return playerUId;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public int getWrong() {
-        return wrong;
-    }
-
-    public ArrayList<String> getWords() {
-        return words;
-    }
 }
+
